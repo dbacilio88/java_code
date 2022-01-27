0896d559b534bc30bd29113c73e48eaf4e948f91
dbacilio88 <dbacilio88@outlook.es>
2022-01-27 02:24:24 -0500

    lectura de archivos

--- archivos/src/main/java/bacsystem/com/archivos/Principal.java
+++ archivos/src/main/java/bacsystem/com/archivos/Principal.java
@@ -4,10 +4,9 @@ import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-//Ctrl + Alt  + l = format code
 public class Principal {
 
-    private final Logger log = Logger.getLogger(Principal.class.getName());
+    private static final Logger log = Logger.getLogger(Principal.class.getName());
 
     public static void main(String[] args) {
 
@@ -17,63 +16,27 @@ public class Principal {
 
     private void getListFiles() {
 
-        File file = new File("C:\\Users\\cbacilio\\Documents\\everis\\desarrollo\\java\\data\\");
+        File file = new File("/home/christian/Documentos/repositories/java_code/data/");
+
+        getSizeFile(file);
 
         File[] files = file.listFiles();
 
         for (String f : file.list()) {
-            log.log(Level.INFO, "Name: {0} Size: {1}", new String[]{f, f.length() + ""});
-
+            log.log(Level.INFO, "Name: {0} Size: {1}", new String[] { f, f.length() + "" });
         }
-        //System.out.println(files.length);
+
+        System.out.println(files.length);
     }
 
     private String getSizeFile(File file) {
-
         String size = null;
-        //if (file.length() > 50000) {
         int k = 1024;
-        String[] format = {"Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+        String[] format = { "Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
         int s = (int) Math.floor(Math.log(file.length() / Math.log(k)));
         System.out.println("file: " + Float.parseFloat(file.length() / Math.pow(k, s) + "") + " " + format[s]);
-        //log.log(Level.INFO, "float {0}", file.length() / Math.pow(k, s));
-
-        //let cantidad = parseFloat((file.size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-        //log.log(Level.INFO, "result {0}", s);
-        //}
-
-
+        log.log(Level.INFO, "float {0}", file.length() / Math.pow(k, s));
+        log.log(Level.INFO, "result {0}", s);
         return size;
     }
-
 }
-
-/**
- * validarSize(file: File) {
- * if (file.size > 500000) {
- * const k = 1024;
- * const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
- * const i = Math.floor(Math.log(file.size) / Math.log(k));
- * const dm = 1;
- * let cantidad = parseFloat((file.size / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
- * this.messageService.add({ severity: 'error', summary: `El archivo no debe superar los 500 KB`, detail: `El archivo tiene: ${cantidad}`, life: 4000 });
- * this.formato_1.controls['vertimientoAguaResidualDocumento'].setValue('')
- * return;
- * }
- * }
- * <p>
- * validarExtension(file: File) {
- * let ext = file.name.split('.').pop();
- * <p>
- * console.log(ext);
- * switch (ext) {
- * case 'jpg':
- * case 'jpeg':
- * case 'png':
- * case 'pdf': break;
- * default:
- * this.messageService.add({ severity: 'error', summary: `El archivo no tiene la extensión adecuada`, detail: `Solo: .jpg, .jpeg, .pdf y .png`, life: 4000 });
- * this.formato_1.controls['vertimientoAguaResidualDocumento'].setValue('')
- * }
- * }
- */
---
+++ archivos/src/main/java/bacsystem/files/Files.java
@@ -0,0 +1,57 @@
+package bacsystem.files;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Files {
+
+    private static final Logger LOGGER = Logger.getLogger(Files.class.getName());
+
+    public static void main(String[] args) {
+
+        File file = new File("/home/christian/Documentos/repositories/java_code/data/application.properties");
+
+        Properties properties = new Properties();
+        InputStream inStream = null;
+        String encontrado = null;
+
+        if (file.isFile()) {
+
+            try {
+                inStream = new FileInputStream(file);
+                properties.load(inStream);
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "files is {0}", e.getMessage());
+            }
+
+            encontrado = encontrarValor(properties.getProperty("values"), "2");
+
+            LOGGER.log(Level.INFO, "el valor es {0}", encontrado);
+        }
+
+    }
+
+    public static String encontrarValor(String values, String encontrar) {
+
+        String result = null;
+
+        List<String> lista = Arrays.asList(values.split("\\|"));
+
+        for (String object : lista) {
+
+            List<String> value = Arrays.asList(object.split("\\,"));
+
+            if (value.get(0).equalsIgnoreCase(encontrar)) {
+                result = value.get(1);
+            }
+        }
+
+        return result;
+    }
+}
